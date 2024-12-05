af2370716e924632b0bf097069893fa0cd93d1b1
Maria Valisheva <dolnish9@gmail.com>
2024-12-05 16:57:04 +0300

    second hw is done

--- src/test/java/GitHubTest.java
+++ src/test/java/GitHubTest.java
@@ -2,6 +2,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class GitHubTest {
@@ -9,7 +11,6 @@ public class GitHubTest {
     @BeforeAll
     static void configParams() {
         Configuration.baseUrl = "https://github.com";
-        Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "2560x1440";
         Configuration.pageLoadStrategy = "eager";
     }
@@ -23,12 +24,16 @@ public class GitHubTest {
         // Перейдите в раздел Wiki проекта
         actions().moveToElement($("#wiki-tab")).click().perform();
 
+        // Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
+        $("#wiki-pages-filter").setValue("SoftAssertions");
+        $(".filterable-active").shouldHave(text("SoftAssertions"));
 
-
+        // Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+        $(".filterable-active").$(byText("SoftAssertions")).click();
+        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
 
     }
 }
 
-// Убедитесь, что в списке страниц (Pages) есть страница SoftAssertions
 
-// Откройте страницу SoftAssertions, проверьте что внутри есть пример кода для JUnit5
+
