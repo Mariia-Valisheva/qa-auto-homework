064ddf71127d43d60d23925cf98a4a702bd3cbee
Maria Valisheva <dolnish9@gmail.com>
2024-12-07 15:54:57 +0300

    third hw is finished

--- src/test/java/GitHubHoverTest.java
+++ src/test/java/GitHubHoverTest.java
@@ -1,9 +1,9 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byTagAndText;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
@@ -14,7 +14,6 @@ public class GitHubHoverTest {
         static void configParams() {
         Configuration.browserSize = "2560x1440";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.holdBrowserOpen = true;
     }
     @Test
     void hoverTest() {
@@ -23,4 +22,27 @@ public class GitHubHoverTest {
         $(byTagAndText("a","Enterprises")).click();
         $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
     }
+
+    //drag and drop with actions
+
+    @Test
+    void dragAndDropTestViaActions() {
+
+       open("https://the-internet.herokuapp.com/drag_and_drop");
+       actions().moveToElement($("#column-a")).clickAndHold().moveToElement($("#column-b")).release().perform();
+       $("#column-a").shouldHave(text("B"));
+    }
+
+    //drag and drop with selenide command
+
+    @Test
+    void dragAndDropTestViaCommand() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+        $("#column-a").shouldHave(text("B"));
+    }
 }
+
+
+
+
