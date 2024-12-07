d9d5104063198452719ba42dd1d4a52b4c60e95d
Maria Valisheva <dolnish9@gmail.com>
2024-12-07 13:59:54 +0300

    enterprises homework

--- src/test/java/GitHubHoverTest.java
+++ src/test/java/GitHubHoverTest.java
@@ -2,17 +2,25 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byTagAndText;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
 public class GitHubHoverTest {
 
     @BeforeAll
         static void configParams() {
-        Configuration.baseUrl = "https://github.com";
         Configuration.browserSize = "2560x1440";
         Configuration.pageLoadStrategy = "eager";
         Configuration.holdBrowserOpen = true;
     }
     @Test
     void hoverTest() {
-
+        open("https://github.com");
+        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+        $(byTagAndText("a","Enterprises")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform"));
     }
 }
