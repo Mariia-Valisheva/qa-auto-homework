492be8ce3b544eb13b74fba63a978deb2c7ebbaf
Maria Valisheva <dolnish9@gmail.com>
2025-03-03 16:49:28 +0300

    hw

---
+++ src/test/java/helpers/Attachments.java
@@ -0,0 +1,58 @@
+package helpers;
+
+import com.codeborne.selenide.Selenide;
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import static com.codeborne.selenide.Selenide.sessionId;
+import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
+import static org.openqa.selenium.logging.LogType.BROWSER;
+
+public class Attachments {
+    @Attachment(value = "{attachName}", type = "image/png")
+    public static byte[] addScreenshot(String attachName) {
+        return ((TakesScreenshot) getWebDriver()).getScreenshotAs(OutputType.BYTES);
+    }
+
+    @Attachment(value = "Page source", type = "text/plain")
+    public static byte[] addPageSource() {
+        return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
+    }
+
+    @Attachment(value = "{attachName}", type = "text/plain")
+    public static String addAttachAsText(String attachName, String message) {
+        return message;
+    }
+
+    public static void addBrowserConsoleLogs() {
+        addAttachAsText(
+                "Browser console logs",
+                String.join("\n", Selenide.getWebDriverLogs(BROWSER))
+        );
+    }
+
+    public static URL getVideoUrl() {
+        String SELENOID_HOST = System.getProperty("selenoid_host");
+        String videoUrl = "https://" + SELENOID_HOST + "/video/" + sessionId() + ".mp4";
+
+        try {
+            return new URL(videoUrl);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+    public static String addVideo() {
+        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
+                + getVideoUrl()
+                + "' type='video/mp4'></video></body></html>";
+    }
+
+}
